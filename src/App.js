414c1bdab90712100e0c9745bc6d14a91b0f4ad4
Tanzimul Hasib <tanzimul.hasib@gmail.com>
2021-03-06 11:48:02 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hey claver Programmer</h1>
+    </div>
+  );
+}
+
+export default App;
+
+// https://api.themoviedb.org/3/movie/550?api_key=0014417194b35b2c9722086a021cd2de
